1e6e20fd6f7fc8818f6ed46381ec31b5b6f870d7
jawadali6688 <jawaddotprogrammer@gmail.com>
2025-01-15 23:21:21 +0500

    flutter app for testing

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Text(
+    "Hello",
+    textAlign: TextAlign.center,
+    textDirection: TextDirection.ltr,
+    style: TextStyle(fontSize: 50, color: Colors.blueAccent),
+  ));
+}
